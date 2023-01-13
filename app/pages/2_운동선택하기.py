850c0dec4daaacc0ad35d90c40cbbc8657c76a24
WonJoon <seowj@gmail.com>
2023-01-13 11:30:31 +0900

    [Feat] streamlit

--- app/pages/2_운동선택하기.py
+++ app/pages/2_운동선택하기.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 import extra_streamlit_components as stx
 
-
 # --- Logout 하면 로그인 화면으로 되돌아가기 ---
 st.sidebar.title(f"Welcome {st.session_state['name']}")
 cookie_manager = stx.CookieManager()
@@ -24,26 +23,47 @@ c1, c2, c3 = st.columns(3)
 with col1:
     st.header("스탠딩 사이드 크런치")
     st.image("img/1.jpg")
-    want_to_contribute = st.button("운동 시작하기")
-    if want_to_contribute:
+    exercise_button = st.button("운동 시작하기1")
+    if exercise_button:
+        st.session_state.num = 0
         switch_page("운동시작하기")
 
 with col2:
     st.header("스탠딩 사이드 크런치")
     st.image("img/1.jpg")
+    exercise_button = st.button("운동 시작하기2")
+    if exercise_button:
+        st.session_state.num = 1
+        switch_page("운동시작하기")
 
 with col3:
     st.header("스탠딩 사이드 크런치")
     st.image("img/1.jpg")
+    exercise_button = st.button("운동 시작하기3")
+    if exercise_button:
+        st.session_state.num = 2
+        switch_page("운동시작하기")
 
 with c1:
     st.header("스탠딩 사이드 크런치")
     st.image("img/1.jpg")
+    exercise_button = st.button("운동 시작하기4")
+    if exercise_button:
+        st.session_state.num = 3
+        switch_page("운동시작하기")
 
 with c2:
     st.header("스탠딩 사이드 크런치")
     st.image("img/1.jpg")
+    exercise_button = st.button("운동 시작하기5")
+    if exercise_button:
+        st.session_state.num = 4
+        switch_page("운동시작하기")
 
 with c3:
     st.header("스탠딩 사이드 크런치")
     st.image("img/1.jpg")
+    exercise_button = st.button("운동 시작하기6")
+    if exercise_button:
+        st.session_state.num = 5
+        switch_page("운동시작하기")
--- app/pages/3_운동시작하기.py
+++ app/pages/3_운동시작하기.py
@@ -1,6 +1,10 @@
 from streamlit_extras.switch_page_button import switch_page
 import streamlit as st
 import extra_streamlit_components as stx
+import cv2
+
+# --- 운동 종류 번호
+st.title(f"Excercise Num : {st.session_state['num']}")
 
 # --- Logout 하면 로그인 화면으로 되돌아가기 ---
 st.sidebar.title(f"Welcome {st.session_state['name']}")
@@ -14,3 +18,17 @@ if st.session_state['authentication_status']:
         st.session_state['username'] = None
         st.session_state['authentication_status'] = None
         switch_page("frontend")
+
+st.title("Pose Estimation with Camera")
+
+
+# --- Camera
+cap = cv2.VideoCapture(0)
+frameST = st.empty()
+
+stopper_started = False
+while True:
+    success, frame = cap.read()
+    if not success: break
+
+    frameST.image(frame, channels="BGR")
