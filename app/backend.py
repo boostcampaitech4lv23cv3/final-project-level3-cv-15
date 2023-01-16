a664a1bf876eb88ed5cd442041bf8f844446d4c2
ss-hj <gkwjd111@naver.com>
2023-01-16 12:15:28 +0000

    [Feat] Sign up & Login API

--- app/backend.py
+++ app/backend.py
@@ -3,8 +3,40 @@ from fastapi.param_functions import Depends
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 from typing import List, Union, Optional, Dict, Any
-
 from datetime import datetime
+from database import engineconn, User, Exercise
 
 app = FastAPI()
 
+engine = engineconn()
+session = engine.sessionmaker()
+
+class UserItem(BaseModel):
+    nickname : str
+    user_id : str
+    user_password : str
+    user_hash : str
+    
+class ExerciseItem(BaseModel):
+    user_hash : str
+    type : str
+    date : datetime
+    count : int
+    perfect : int
+    good : int
+    miss : int
+
+@app.get("/users")
+def get_user():
+    example = session.query(User).all()
+    return example
+
+@app.post("/users")
+async def register_user(item: UserItem):
+    new_user = User(nickname=item.nickname, 
+                   user_id=item.user_id,
+                   user_password=item.user_password, 
+                   user_hash=item.user_hash)
+    session.add(new_user)
+    session.commit()
+    return new_user
--- app/frontend.py
+++ app/frontend.py
@@ -1,71 +1,61 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 from streamlit_extras.switch_page_button import switch_page
-
-# --- USER AUTHENTICATION(나중에 db로 연결) ---
-names = ["DH", "HJ"]
-usernames = ["dh", "hj"]
-passwords = ["1234", "1111"]
-hash_pw = stauth.Hasher(passwords).generate()
-credentials = {"usernames":{}}
-
+import requests
+from backend import get_user
 
 # --- LINK TO THE CSS FILE ---
 with open('style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 
-def loginPage():
+col1, col2 = st.columns(2)
+with col1:
     # --- Login Page
     st.title("LOGIN")
 
     login = st.form("login form")
     username = login.text_input("User name")
     pw = login.text_input("Password", type='password')
-    submit = login.form_submit_button("submit")
+    login_submit = login.form_submit_button()
 
-
-def signupPage():
+    if login_submit:
+        users = get_user()
+        for u in users:
+            if u.user_id == username:
+                if u.user_password == pw:
+                    # TODO : 사용자의 암호화된 비밀번호를 다른 페이지에서도 접근가능하도록 설정
+                    hashed_passwords = u.user_hash
+                    print(hashed_passwords)
+                else: # 해당 id에 일치하는 비밀번호가 아닌 경우
+                    print("Incorrect password.")
+                break
+        else: # 존재하지 않는 user_id인 경우
+            print("Please create an account first.")
+            
+with col2:
     # --- Signup Page
     st.title("SIGN UP")
 
     login = st.form("signup form")
-    username = login.text_input("User name")
+    username = login.text_input("User id")
     pw = login.text_input("Password", type='password')
     nickname = login.text_input("Nick name")
-    submit = login.form_submit_button("submit")
-
-
-col1, col2 = st.columns(2)
-with col1:
-    st.button(
-        label="Sign in",
-        on_click=loginPage,
-    )
-with col2:
-    st.button(
-        label="Sign up",
-        on_click=signupPage,
-    )
-
-
-"""
-# --- LOGIN AUTHENTICATION ---
-for un, name, pw in zip(usernames, names, hash_pw):
-    user_dict = {"name":name,"password":pw}
-    credentials["usernames"].update({un:user_dict})
-
-authenticator = stauth.Authenticate(credentials, "ck_name", "abcd", cookie_expiry_days=30)
-
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-
-if authentication_status:
-    authenticator.logout("Logout", "sidebar")
-    switch_page("운동선택하기")  # 로그인 하면 운동 선택 페이지로 이동
-"""
+    signup_submit = login.form_submit_button()
+    
+    if signup_submit:
+        hashed_passwords = stauth.Hasher([pw]).generate()[0]
+        new_user = {"user_password": pw,
+                    "user_id": username,
+                    "user_hash": hashed_passwords,
+                    "nickname": nickname}
+        users = get_user()
+        new = True
+        for u in users:
+            if u.user_id == username:
+                new = False
+                break
+        if new: # 기존의 DB에 존재하지 않는 user_id인 경우
+            requests.post('http://127.0.0.1:8000/users', json=new_user)
+        else:
+            print("Identical ID already exists.")
